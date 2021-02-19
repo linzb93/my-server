12658a8bbbfa64bcbc55ea6167da98adafcad27a
linzb <linzb93@qq.com>
2021-02-19 08:22:27 +0800

    新增本地图片服务器

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs-extra');
+const path = require('path');
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const {errorLogger} = require('./utils/utils');
-
+const resolve = src => path.resolve(__dirname, src);
 (async () => {
   process.on('unhandledRejection', e => {
     errorLogger(e);
@@ -13,22 +14,18 @@ const {errorLogger} = require('./utils/utils');
   app.use(cors());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use('/public', express.static('./files'));
+  app.use('/public', express.static(resolve('files')));
   let config;
   try {
-    config = await fs.readJSON('./config.json', 'utf8');
+    config = await fs.readJSON(resolve('config.json'), 'utf8');
   } catch (e) {
     errorLogger(e);
     return;
   }
   const {projects} = config;
-  if (!projects.length) {
-    errorLogger('当前没有项目可以加载，退出');
-    return;
-  }
   if (projects.length === 1) {
     require(`./projects/${projects[0].id}`)(app);
-  } else {
+  } else if (projects.length > 1) {
     const {answer} = await inquirer.prompt([{
       type: 'list',
       name: 'answer',
--- projects/adSystem/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('./router');
-const createProjectDB = require('../../service/createProjectDB');
-
-module.exports = async app => {
-  await createProjectDB(__dirname);
-  app.use('/', router);
-};
